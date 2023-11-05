30aa254e08315aa277177f345ba5707ee4c27693
João Vitor Vogel Vieira <joao0404200@gmail.com>
2023-11-05 19:10:55 -0300

    Create Localidade.java

---
+++ src/Localidade.java
@@ -0,0 +1,32 @@
+public class Localidade{
+        private String cidade;
+        private String estado;
+        private String nomeEstrada;
+        private int numero;
+        private int longitude;
+        private int latitude;
+
+        public String getCidade() {
+            return cidade;
+        }
+
+        public void setCidade(String cidade) {
+            this.cidade = cidade;
+        }
+
+        public String getEstado() {
+            return estado;
+        }
+
+        public void setEstado(String estado) {
+            this.estado = estado;
+        }
+
+        public String getNomeEstrada() {
+            return nomeEstrada;
+        }
+        public void setNomeEstrada(String nomeEstrada) {
+            this.nomeEstrada = nomeEstrada;
+        }
+}
+
